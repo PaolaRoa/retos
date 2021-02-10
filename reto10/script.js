18e387df96f058ed1f1e9575cbab1312e8cff40c
Paola R <yproab@gmail.com>
2021-02-09 19:38:59 -0500

    reto10

--- reto10/script.js
+++ reto10/script.js
@@ -1,8 +1,27 @@
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll('.form-control label');
+let transition = 0;
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span>${letter}</span>`)
+        .map((letter) => `<span>${letter}</span>`)
         .join('')
+        console.log(label)
 })
+
+
+
+
+
+labels.forEach( label =>{
+    transition = 100;
+    label.childNodes.forEach(sp =>{
+        transition += 100;
+        sp.style.transitionDuration = `${transition}ms`
+    }
+
+    )
+}
+
+    )
+    
