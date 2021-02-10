60f4a5f99f0714417b3a6e8b42a2b45601fa9ba8
pcProgramate <paolaroa1925@gmail.com>
2021-02-10 12:31:02 -0500

    reto13

--- reto13/script.js
+++ reto13/script.js
@@ -6,22 +6,21 @@ getData()
 
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
-function getData() {
-    const res = 
+async function getData() {
+    
+    const res = await fetch("https://randomuser.me/api?results=50")
 
-    const {  } = json()
-
+    const {results, info} = await res.json()
+    // console.log(results)
     // Clear result
     result.innerHTML = ''
 
-    (user => {
+    results.forEach(user => {
         const li = document.createElement('li')
-
+        console.log(user)
         listItems.push(li)
 
-        li.innerHTML = `
- 
-        `
+        li.innerHTML = `<img class="photo" src= "${user.picture.thumbnail}"><img><div class= "user-info"><h4>${user.name.first} ${user.name.last}</h4><p>${user.location.city}, ${user.location.country}</p></div>`
 
         result.appendChild(li)
     })
