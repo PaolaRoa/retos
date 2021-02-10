6a07aa3f56623510afb56223bd001a1bd178dd73
pcProgramate <paolaroa1925@gmail.com>
2021-02-10 18:09:51 -0500

    14

---
+++ reto14/script.js
@@ -0,0 +1,31 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        // console.log(x,y);
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        console.log(buttonTop,buttonLeft);
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+        
+        console.log(xInside,yInside);
+
+        if(xInside > 0 && yInside > 0){
+            console.log(e.target)
+            e.target.classList.add("circle")
+        }
+
+    })
+})
+
+
+
+
+
