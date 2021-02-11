444bb5f7c09625cef33d194e9d4c90ade2fbb02b
pcProgramate <paolaroa1925@gmail.com>
2021-02-11 12:17:36 -0500

    reto16

---
+++ landingPage/script.js
@@ -0,0 +1,24 @@
+
+const list = document.getElementById("lista")
+let url = ``
+let retosurl = []
+
+
+
+for(i=1; i<26; i++){
+    url = `../../reto${i}/index.html`;
+    retosurl.push(url);  
+}
+
+const createList = () =>{
+    retosurl.map(url => {
+        item = document.createElement("li");
+        item.innerHTML = `<a href="${url}" target="blank">Reto # ${retosurl.indexOf(url)+1}</a>`;
+        list.appendChild(item)
+        // console.log(item)
+    })
+} 
+
+createList()
+
+console.log(retosurl)
