4af5fd3b493f5aacf24cb80832639fbaa6fce709
pcProgramate <paolaroa1925@gmail.com>
2021-02-12 12:47:34 -0500

    reto22

--- reto22/Readme.md
+++ reto22/Readme.md
@@ -4,9 +4,11 @@ Bienvenidos al reto Mobile Tab Navigation, para cumplirlo deberán seguir lo sig
 
 1. Ver el video de explicación de reto.
 
-2. Tienen los siguientes recursos: Entregamos el HTML con las clases correspondientes, css básico y js con la estructura que deben seguir*
+2. Tienen los siguientes recursos: Entregamos el HTML con las clases correspondientes,
+ css básico y js con la estructura que deben seguir*
    
-   * En este caso les damos una estructura guía donde deben encontrar las variables e identificadores necesarios en cada caso.
+   * En este caso les damos una estructura guía donde deben encontrar las variables 
+   e identificadores necesarios en cada caso.
 
 3. Deberán completar el css y el js para que el reto funcione.
 
--- reto22/script.js
+++ reto22/script.js
@@ -1,18 +1,26 @@
-const contents = document.querySelectorAll('')
-const listItems = document.querySelectorAll('')
+const contents = document.querySelectorAll('img')
+const listItems = document.querySelectorAll('li')
+console.log(contents)
 
 listItems.forEach((item, idx) => {
-    item.addEventListener('', () => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+        item.classList.add('active');
+        contents[idx].classList.add('show')
 
-        item.classList.add('')
+        
     })
 })
 
 function hideAllContents() {
-
+    contents.forEach(content =>
+        content.classList.remove('show'))
 }
 
 
 function hideAllItems() {
- 
+    listItems.forEach((item)=>{
+        item.classList.remove('active')
+    })
 }
