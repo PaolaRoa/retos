13721a799a840a0b3c696662e89bf0a14cb5925f
pcProgramate <paolaroa1925@gmail.com>
2021-02-12 14:21:47 -0500

    reto 23

--- reto23/Readme.md
+++
@@ -1,4 +0,0 @@
-JS
-
-- Debes crear operaciones condicionales según la cantidad de agua en el baso
-- Tienes las variables declaradas y listas para usar
--- reto23/script.js
+++ reto23/script.js
@@ -1,5 +1,5 @@
 const smallCups = document.querySelectorAll('.cup-small')
-const listers = document.getElementById('liters')
+const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
@@ -13,13 +13,34 @@ function highlightCups(idx) {
 
 
     smallCups.forEach((cup, idx2) => {
-
+        if (idx2 <= idx){
+        cup.classList.add('full')}
     })
 
     updateBigCup()
 }
 
 function updateBigCup() {
-    const fullCups = document.querySelectorAll('.cup-small.full').length
-    const totalCups = smallCups.length
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+    
+
+    const per = (fullCups *100) /totalCups;
+    if (per > 0){
+        percentage.innerText= per +"%"
+    }
+    
+    percentage.style.height = per + '%';
+    if (per == 100){
+        remained.style.height = 0;
+    }
+
+    const remain = 2 - (fullCups*0.25)
+    
+    if (remain > 0){
+        liters.innerText = remain +'L'
+    }
+
+
+
 }
