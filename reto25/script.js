592dd010fb42b4c5ef2f088eca96d78a947eee38
pcProgramate <paolaroa1925@gmail.com>
2021-02-12 17:02:55 -0500

    finished retos

--- reto25/script.js
+++ reto25/script.js
@@ -1,12 +1,24 @@
-const nav = document.querySelector('.navbar')
-window.addEventListener('scroll', fixNav)
+const nav = document.querySelector('.navbar');
+const list = document.querySelectorAll('li');
+const log = document.querySelector('.logo')
+
+console.log(log)
+window.addEventListener('scroll', fixNav);
+
 
 function fixNav() {
     console.log(window.scrollY)
     if(window.scrollY > nav.offsetHeight+ 150) {
-        nav.classList.add('onscroll')}
+        nav.classList.add('onscroll');
+        list.forEach(item=> item.classList.add('act'))
+        log.classList.add('act')
+      }
 
     else {
-      nav.classList.remove('onscroll')
+      nav.classList.remove('onscroll');
+      list.forEach(item=> item.classList.remove('act'))
+      log.classList.remove('act')
+
+      
     }
 }
